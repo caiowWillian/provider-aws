49401a39b9323f375e0176e0c7d866456a52c82f
Caio Willian <caiow.willian@gmail.com>
2022-01-20 16:49:19 -0300

    chore: initial apigateway code generation
    Signed-off-by: Caio Willian <caiow.willian@gmail.com>

---
+++ apis/apigateway/v1alpha1/custom_types.go
@@ -0,0 +1,17 @@
+package v1alpha1
+
+type CustomAPIKeyParameters struct{}
+type CustomAuthorizerParameters struct{}
+type CustomBasePathMappingParameters struct{}
+type CustomDeploymentParameters struct{}
+type CustomDocumentationPartParameters struct{}
+type CustomDocumentationVersionParameters struct{}
+type CustomDomainNameParameters struct{}
+type CustomModelParameters struct{}
+type CustomRequestValidatorParameters struct{}
+type CustomResourceParameters struct{}
+type CustomRestAPIParameters struct{}
+type CustomStageParameters struct{}
+type CustomUsagePlanKeyParameters struct{}
+type CustomUsagePlanParameters struct{}
+type CustomVPCLinkParameters struct{}
